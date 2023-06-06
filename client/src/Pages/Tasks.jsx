5fcfc946005294a7b60334436714d1392e3a6f6a
Naseeh AN <naseehan700@gmail.com>
2023-06-06 16:17:47 +0530

    change url login

--- client/src/Pages/Tasks.jsx
+++ client/src/Pages/Tasks.jsx
@@ -21,7 +21,7 @@ const [success, setSuccess] = useState(false)
 const handleSubmit = async (e) => {
   e.preventDefault()
   try {
-    await axios.post('http://localhost:3001/task', { name, description, date, member })
+    await axios.post(`${process.env.REACT_APP_BASE_URL}/task`, { name, description, date, member })
     console.log('Task saved successfully');
     setName('')
     setDescription('')
